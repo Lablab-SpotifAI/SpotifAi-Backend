fb7e1c739db0e163cc184cc671785d47d6f53774
Achraf OUJJIR <oujjir.achraf@gmail.com>
2023-09-07 17:54:00 +0000

    gitignore

--- apikey_test.py
+++
@@ -1,4 +0,0 @@
-import openai
-
-chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
-print(chat_completion.choices[0].message.content)
